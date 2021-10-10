d1dd72c3181b1fdb505369ab42315ee13b634f56
lunarbridge <fibremint@gmail.com>
2021-10-10 14:17:04 +0900

    Update argument parser

--- components/mfa-aligner/entrypoint.py
+++ components/mfa-aligner/entrypoint.py
@@ -3,9 +3,11 @@ import subprocess
 import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--mfa-input-path', type=str, default='')
-parser.add_argument('--lexicon-path', type=str, default='')
-parser.add_argument('--mfa-output-path', type=str, default='')
+parser.add_argument('--current-data-path', type=str, default='', required=True)
+parser.add_argument('--fs2-base-path', type=str, default='/mnt/fs2-data')
+parser.add_argument('--mfa-input-path', type=str, default='./before-align')
+parser.add_argument('--mfa-output-path', type=str, default='./aligned')
+parser.add_argument('--lexicon-path', type=str, default='./common/lexicons/librispeech-lexicon.txt')
 opt = parser.parse_args()
 
 cpu_threads = str(os.cpu_count())
@@ -13,9 +15,9 @@ cpu_threads = str(os.cpu_count())
 # ref: https://stackoverflow.com/questions/89228/how-to-execute-a-program-or-call-a-system-command
 subprocess.run(['mfa',
                 'align',
-                opt.mfa_input_path,
-                opt.lexicon_path,
+                os.path.join(opt.current_data_path, opt.mfa_input_path),
+                os.path.join(opt.fs2_base_path, opt.lexicon_path),
                 'english',
-                opt.mfa_output_path,
+                os.path.join(opt.current_data_path, opt.mfa_output_path),
                 '-j',
                 cpu_threads])
