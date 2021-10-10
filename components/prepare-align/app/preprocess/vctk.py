643186f952818c5fd3114b19bd648344b4ed0550
lunarbridge <fibremint@gmail.com>
2021-10-10 11:32:28 +0900

    Clean codes

--- components/prepare-align/app/preprocess/vctk.py
+++ components/prepare-align/app/preprocess/vctk.py
@@ -1,22 +1,20 @@
 import os
+import re
 from collections import defaultdict
 from itertools import groupby
+from typing import List
+
 import librosa
 import numpy as np
-import pandas as pd
-import re
-import glob
-from typing import List
+from joblib import Parallel, cpu_count, delayed
 from scipy.io import wavfile
-from tqdm import tqdm
-from joblib import Parallel, delayed, cpu_count
-import json
-
 from text import _clean_text
+from tqdm import tqdm
 
 
 def work(input_path, input_wav_path, txt_file, out_dir, cleaners, sampling_rate, max_wav_value):
     # get base name
+    
     base_name = os.path.basename(input_wav_path).split('.')[0]
     speaker = input_wav_path.split('/')[1]
 
@@ -54,40 +52,19 @@ def work(input_path, input_wav_path, txt_file, out_dir, cleaners, sampling_rate,
 
 def prepare_align(data_relpaths: List, input_path: str, out_dir: str,
                   sample_rate: int, max_wav_value: float, cleaners: List):
-    # load train speakers
-    # with open(train_speakers, 'r') as r:
-    #     train_spks = [l.strip() for l in r.readlines()]
-
-    # List-up all wav files
-    # wav_files = glob.glob(f'{in_dir}/wav48/*/*.wav')
-
-    # List-up text files
-    # txt_files = glob.glob(f'{in_dir}/**/*.txt', recursive=True)
-
-    # Match wav and text files, filter speakers
+    # Match wav and text files
     data_relpaths_dict = defaultdict(dict)
-    # for key, group in groupby(data_relpaths, lambda item: item.split('/')[0]):
     for key, group in groupby(data_relpaths, lambda item: os.path.basename(item).split('.')[0]):
         filepath = list(group)[0]
         filetype = os.path.basename(filepath).split('.')[1]
         data_relpaths_dict[key][filetype] = filepath
-
-    # info = {os.path.basename(file_path).split('.')[0]: {'wav_file': file_path} for file_path in wav_files}
-    # for text_file_path in txt_files:
-    #     key = os.path.basename(text_file_path).split('.')[0]
-    #     # filter train spks
-    #     # if key.split('_')[0] not in train_spks:
-    #     #     continue
-    #     if key in info:
-    #         info[key]['txt_file'] = text_file_path
-
 
     # re-write
     input_wav_list = []
     input_txt_list = []
     for key, files in data_relpaths_dict.items():
         if len(files) == 1:
-            print(f"WARN: data '{key}' is not sufficient. maybe lack of the wav or txt file. skipped.")
+            print(f"WARN: data '{key}' is not sufficient. maybe lack of the 'wav' or 'txt' file. skipped.")
             continue
         wav, txt_file = files['wav'], files['txt']
         input_wav_list.append(wav)
